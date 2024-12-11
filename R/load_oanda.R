bd0a847c83ec58e29ec7e75db3974e00ffecffa8
Meichtry Samuel BJ <samuel.meichtry@bj.admin.ch>
2024-12-11 08:22:27 +0100

    Implement a simple caching

--- R/load_oanda.R
+++ R/load_oanda.R
@@ -10,6 +10,11 @@
 #' fxPairs <- c("USDCHF","EURCHF", "GBPCHF", "CADCHF")
 #' load_oanda(fxPairs)
 load_oanda <- function(fxPairs, path = "../forex_oanda"){
+  # Load Cache
+  if(exists("dataOANDA")){
+    if (all(fxPairs %in% names(dataOANDA)[-1]))  return(dataOANDA)
+  }
+
   # Read each forex pair
   listFX <- get_oanda(fxPairs)
 
